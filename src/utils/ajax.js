da450693f953842715a99c00509312abcf04d6c4
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2020-09-07 15:33:42 -0700

    make https

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -3,13 +3,13 @@ import axios from "axios";
 export default {
   getMovies(searchInput) {
     return axios.get(
-      `http://www.omdbapi.com/?apikey=951cc99b&s=${searchInput}&type=movie`
+      `https://www.omdbapi.com/?apikey=951cc99b&s=${searchInput}&type=movie`
     );
   },
 
   getMovie(movieID) {
     return axios.get(
-      `http://www.omdbapi.com/?apikey=951cc99b&i=${movieID}&type=movie`
+      `https://www.omdbapi.com/?apikey=951cc99b&i=${movieID}&type=movie`
     );
   },
 };
